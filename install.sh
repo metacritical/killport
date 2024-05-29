e9a10560e79b5e71b7d467986f9b7476217d6791
Pankaj Doharey <pankajdoharey@gmail.com>
2024-05-30 03:24:01 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 OS=$(uname)
 
 REPO="https://github.com/metacritical/killport"
-LATEST_RELEASE_URL="$REPO/releases/download/0.0.1/kp"
+LATEST_RELEASE_URL="$REPO/releases/download/v0.0.1/killport"
 INSTALL_DIR="/usr/local/bin"
 
 echo "Installing killport..."
